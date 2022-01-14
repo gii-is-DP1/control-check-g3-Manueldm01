1ab322261bd4cdf8f87ba5d4a01f7cb9dc4207bb
Manuel <manueldominguezmagdaleno@gmail.com>
2022-01-14 16:39:19 +0100

    Examen

--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoom.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoom.java
@@ -1,17 +1,42 @@
 package org.springframework.samples.petclinic.recoveryroom;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Transient;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class RecoveryRoom {
-    Integer id;
-    String name;
+@Entity
+@Table(name="recoveryrooms")
+public class RecoveryRoom extends BaseEntity{
+
+	
+    @Size(min = 3, max = 50)
+	@Column(name = "name")
+    @NotNull
+	private String name;
+    
+    @NotNull
+    @Min(0)
     double size;
+    
+    @NotNull
     boolean secure;
+    
+    
     @Transient
+	@ManyToOne(optional = false)
+	@JoinTable(name="roomtype_id")
     RecoveryRoomType roomType;
 }
--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomService.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomService.java
@@ -2,25 +2,33 @@ package org.springframework.samples.petclinic.recoveryroom;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RecoveryRoomService {
+	
+	@Autowired
+	RecoveryRoomRepository recoveryroomrepo;
+	
     public List<RecoveryRoom> getAll(){
-        return null;
+        return this.recoveryroomrepo.findAll();
     }
 
     public List<RecoveryRoomType> getAllRecoveryRoomTypes(){
-        return null;
+        return this.recoveryroomrepo.findAllRecoveryRoomTypes();
     }
 
     public RecoveryRoomType getRecoveryRoomType(String typeName) {
-        return null;
+        return this.recoveryroomrepo.getRecoveryRoomType(typeName);
     }
 
     public List<RecoveryRoom> getRecoveryRoomsBiggerThan(double size) {
-        return null;
+        return this.recoveryroomrepo.findBySizeMoreThan(size);
     }
 
     public RecoveryRoom save(RecoveryRoom p) {
-        return null;       
+        return this.recoveryroomrepo.save(p);       
     }
 
     
